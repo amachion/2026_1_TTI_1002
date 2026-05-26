89f2fe9fa4f64955f9b591d0ebfdb4a851c89d12
andreia.machion <andreia.machion@SP-B13-P-37246.maua.br>
2026-05-26 10:42:51 -0300

    Implementação de select, delete e update

--- src/main/java/Musica.java
+++ src/main/java/Musica.java
@@ -1,10 +1,12 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Musica {
     private String titulo;
     private int duracao; //em segundos
     private double avaliacao;
+    public Musica () {} //a JVM inicializa cada atributo com seu valor padrão
     public Musica (String titulo) {
         this.titulo = titulo;
     }
@@ -17,6 +19,10 @@ public class Musica {
         // this.titulo = titulo;
         // this.duracao = duracao;
         this(titulo, duracao);
+        setAvaliacao(avaliacao);
+    }
+    public Musica (String titulo, double avaliacao) {
+        this(titulo);
         setAvaliacao(avaliacao);
     }
     public void setAvaliacao (double avaliacao) {
@@ -58,11 +64,11 @@ public class Musica {
     // iniciar os métodos para conversar com o banco
     public void cadastrar() {
         //1. definir o insert como uma String
-        String sql = "INSERT INTO tb_musica (titulo, duracao, avaliacao)"+
+        String sql = "INSERT INTO tb_musica (titulo, duracao, avaliacao) "+
                "VALUES (?, ?, ?)";
-        //2. Abrir uma conexão
+        //2. Invocando a fábrica de conexões
         ConnectionFactory factory = new ConnectionFactory();
-        //try com recursos: são liberados automaticamente
+        //try com recursos: são liberados automaticamente = pede a conexão
         try (Connection c = factory.obtemConexao()){
             //2.1 pré compilar o comando sql
             PreparedStatement ps = c.prepareStatement(sql);
@@ -76,6 +82,61 @@ public class Musica {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public void atualizar () {
+        //1 - Definir a string de atualização (só para avaliação)
+        String sql = "UPDATE tb_musica SET avaliacao = ? WHERE titulo = ?";
+        //2 - Invoca a fábrica de conexões
+        ConnectionFactory factory = new ConnectionFactory();
+        //2.1 obtem a conexão, dentro do try
+        try (Connection c = factory.obtemConexao()) {
+            //2.2 pre-compila
+            PreparedStatement ps = c.prepareStatement(sql);
+            //3 - preencher os placehlders
+            ps.setDouble(1, avaliacao);
+            ps.setString(2, titulo);
+            //4 - executar
+            ps.execute();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void remover() {
+        String sql = "DELETE FROM tb_musica WHERE titulo = ?";
+        ConnectionFactory factory = new ConnectionFactory();
+        try (Connection c = factory.obtemConexao()) {
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, titulo);
+            ps.execute();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String listar () {
+        String sql = "SELECT * FROM tb_musica";
+        ConnectionFactory factory = new ConnectionFactory();
+        try (Connection c = factory.obtemConexao()) {
+            PreparedStatement ps = c.prepareStatement(sql);
+            //para receber o resultado, temos um conjunto de resultados
+            ResultSet rs = ps.executeQuery();
+            //iterar sobre o conjunto
+            String saidaFinal = "";
+            while (rs.next()) {
+                String titulo = rs.getString("titulo");
+                int duracao = rs.getInt("duracao");
+                double avaliacao = rs.getDouble("avaliacao");
+                String saida = String.format ("Titulo: %s, Duração: %d, Avaliação: %.2f\n",
+                        titulo, duracao, avaliacao);
+                saidaFinal += saida;
+            }
+            return saidaFinal;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "set list vazio";
     }
 }
 
